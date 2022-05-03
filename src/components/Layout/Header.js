2e7cce0acc5a7a1aa07e388615fc14b24d9b7d7b
Nisarg Patel <np96380@gmail.com>
2022-05-02 17:31:15 -0700

    UI redesign init using tailwind

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-import foodBanner from '../../assets/Food_Order_Banner.jpg';
-import classes from './Header.module.css';
+import foodBanner from "../../assets/Food_Order_Banner.png";
+import foodBannerLite from "../../assets/Food_Order_Banner_l.png";
+import backdropImg from "../../assets/backdrop.png";
+// import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = props => {
+const Header = (props) => {
   return (
     <React.Fragment>
-      <header className={classes.header}>
-        <h1>Foodzie</h1>
+      <header className="bg-zinc-800 p-3">
+        <h1 className="text-2xl font-bold text-green-600">Foodzie</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes['main-image']}>
+      <div>
+        <img src={backdropImg} alt="BackgroundImg" />
         <img src={foodBanner} alt="BackgroundImg" />
       </div>
     </React.Fragment>
