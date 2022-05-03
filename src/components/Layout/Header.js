743147bf255d0316ce275c82436ccd4334436693
Nisarg Patel <np96380@gmail.com>
2022-05-02 21:21:12 -0700

    added dark mode

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,56 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
-import foodBanner from "../../assets/Food_Order_Banner.png";
-import foodBannerLite from "../../assets/Food_Order_Banner_l.png";
+import foodBanner from "../../assets/Food_bg.png";
+import foodBanner_l from "../../assets/Food_bg_l.png";
 import backdropImg from "../../assets/backdrop.png";
+import backdropImg_l from "../../assets/backdrop_l.png";
 // import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
+  const [toggleBanner, setToggleBanner] = useState(true);
+
+  const toggleHandler = () => {
+    document.body.classList.toggle("dark");
+    document.body.classList.contains("dark")
+      ? setToggleBanner(true)
+      : setToggleBanner(false);
+  };
   return (
     <React.Fragment>
-      <header className="bg-zinc-800 p-3">
+      <header className="bg-zinc-800 p-3 z-50">
         <h1 className="text-2xl font-bold text-green-600">Foodzie</h1>
         <HeaderCartButton onClick={props.onShowCart} />
+        <button
+          onClick={toggleHandler}
+          className="bg-gray-300 text-green-800 p-2 absolute right-8 top-2 rounded-md"
+        >
+          Toggle Dark!
+        </button>
       </header>
       <div>
-        <img src={backdropImg} alt="BackgroundImg" />
-        <img src={foodBanner} alt="BackgroundImg" />
+        {toggleBanner && (
+          <img
+            src={backdropImg}
+            alt="BackgroundImg"
+            className="absolute z-10"
+          />
+        )}
+        {toggleBanner && (
+          <img src={foodBanner} alt="BackgroundImg" className="z-0" />
+        )}
+
+        {!toggleBanner && (
+          <img
+            src={backdropImg_l}
+            alt="BackgroundImg"
+            className="absolute z-10"
+          />
+        )}
+        {!toggleBanner && (
+          <img src={foodBanner_l} alt="BackgroundImg" className="z-0" />
+        )}
       </div>
     </React.Fragment>
   );
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,8 +1,8 @@
-import classes from './MealsSummary.module.css';
+import classes from "./MealsSummary.module.css";
 
 const MealsSummary = () => {
   return (
-    <section className={classes.summary}>
+    <section className="flex z-50 bg-slate-800 dark:bg-slate-300 text-green-300 dark:text-green-800 rounded-md absolute top-1/4 right-40 w-2/5 p-8 flex-col text-center">
       <h2>Delicious Food, Delivered To You</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
