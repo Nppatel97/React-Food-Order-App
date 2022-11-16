ffd898c7d0928dd4f7e46a7caf63fbb9eafa07fc
Nisarg Patel <np96380@gmail.com>
2022-11-16 12:17:33 +0530

    customized dark mode a little more

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,8 +3,6 @@ import { useState } from "react/cjs/react.development";
 
 import foodBanner from "../../assets/Food_bg.png";
 import foodBanner_l from "../../assets/Food_bg_l.png";
-import backdropImg from "../../assets/backdrop.png";
-import backdropImg_l from "../../assets/backdrop_l.png";
 // import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
@@ -19,35 +17,22 @@ const Header = (props) => {
   };
   return (
     <React.Fragment>
-      <header className="bg-zinc-800 p-3 z-50">
+      <header className="dark:bg-zinc-800 bg-zinc-300 p-3 z-50">
         <h1 className="text-2xl font-bold text-green-600">Foodzie</h1>
         <HeaderCartButton onClick={props.onShowCart} />
         <button
           onClick={toggleHandler}
-          className="bg-gray-300 text-green-800 p-2 absolute right-8 top-2 rounded-md"
+          className="bg-gray-800 dark:bg-gray-500 text-green-800 p-1 text-xl absolute right-8 top-2 rounded-full"
         >
-          Toggle Dark!
+          {toggleBanner && <span>🔆</span>}
+          {!toggleBanner && <span>🌙</span>}
         </button>
       </header>
       <div>
         {toggleBanner && (
-          <img
-            src={backdropImg}
-            alt="BackgroundImg"
-            className="absolute z-10"
-          />
-        )}
-        {toggleBanner && (
           <img src={foodBanner} alt="BackgroundImg" className="z-0" />
         )}
 
-        {!toggleBanner && (
-          <img
-            src={backdropImg_l}
-            alt="BackgroundImg"
-            className="absolute z-10"
-          />
-        )}
         {!toggleBanner && (
           <img src={foodBanner_l} alt="BackgroundImg" className="z-0" />
         )}
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,6 +1,6 @@
 const MealsSummary = () => {
   return (
-    <section className="flex z-50 bg-slate-800 dark:bg-slate-300 text-green-300 dark:text-green-800 rounded-md absolute top-1/4 right-40 w-2/5 p-8 flex-col text-center">
+    <section className="flex items-center justify-center w-full text-center md:w-2/4 bg-slate-800/80 dark:bg-slate-300/80 text-gray-300 dark:text-gray-800 rounded-md md:absolute top-1/4 left-1/4 p-12 flex-col">
       <h2>Delicious Food, Delivered To You</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
